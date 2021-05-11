4837adf1a8ba4a30156dae37fe07bd158b8770b7
Shubham Mahajan <shubham.mahajan@icloud.com>
2021-05-11 19:38:18 -0400

    program for addition

---
+++ Basic-Programs/src/Addition.java
@@ -0,0 +1,16 @@
+
+public class Addition {
+
+	public void add()
+	{
+		int a = 5, b = 7;
+		a = a + b;
+		System.out.println("The addition of a and b is: " + a);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition additionObject = new Addition();
+		additionObject.add();
+	}
+
+}
